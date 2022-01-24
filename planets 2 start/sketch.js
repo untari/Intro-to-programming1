ae8e544e6159d8aa6d9046df0e038db4e9b895cb
untari <sastraembun0306@gmail.com>
2022-01-24 17:45:35 +0700

    travising data using for loop

--- planets 2 start/sketch.js
+++ planets 2 start/sketch.js
@@ -15,43 +15,51 @@ function draw()
 	textAlign(CENTER);
 	fill(255);
 
-	fill(colours[0]);
-	ellipse(50, height / 2, sizes[0]);
-	fill(255);
-	text(names[0], 50, height / 2 + 150);
+	for(var i = 0; i < 8; i++)
+	{
+		fill(colours[i]);
+		ellipse(50 + i*125, height/2, sizes[i]);
+		fill(255);
+		text(names[i], 50 + i*125, height/2 + 150);
+	}
 
-	fill(colours[1]);
-	ellipse(175, height / 2, sizes[1]);
-	fill(255);
-	text(names[1], 175, height / 2 + 150);
+	// fill(colours[0]);
+	// ellipse(50, height / 2, sizes[0]);
+	// fill(255);
+	// text(names[0], 50, height / 2 + 150);
 
-	fill(colours[2]);
-	ellipse(300, height / 2, sizes[2]);
-	fill(255);
-	text(names[2], 300, height / 2 + 150);
+	// fill(colours[1]);
+	// ellipse(175, height / 2, sizes[1]);
+	// fill(255);
+	// text(names[1], 175, height / 2 + 150);
 
-	fill(colours[3]);
-	ellipse(425, height / 2, sizes[3]);
-	fill(255);
-	text(names[3], 425, height / 2 + 150);
+	// fill(colours[2]);
+	// ellipse(300, height / 2, sizes[2]);
+	// fill(255);
+	// text(names[2], 300, height / 2 + 150);
 
-	fill(colours[4]);
-	ellipse(550, height / 2, sizes[4]);
-	fill(255);
-	text(names[4], 550, height / 2 + 150);
+	// fill(colours[3]);
+	// ellipse(425, height / 2, sizes[3]);
+	// fill(255);
+	// text(names[3], 425, height / 2 + 150);
 
-	fill(colours[5]);
-	ellipse(675, height / 2, sizes[5]);
-	fill(255);
-	text(names[5], 675, height / 2 + 150);
+	// fill(colours[4]);
+	// ellipse(550, height / 2, sizes[4]);
+	// fill(255);
+	// text(names[4], 550, height / 2 + 150);
 
-	fill(colours[6]);
-	ellipse(800, height / 2, sizes[6]);
-	fill(255);
-	text(names[6], 800, height / 2 + 150);
+	// fill(colours[5]);
+	// ellipse(675, height / 2, sizes[5]);
+	// fill(255);
+	// text(names[5], 675, height / 2 + 150);
 
-	fill(colours[7]);
-	ellipse(925, height / 2, sizes[7]);
-	fill(255);
-	text(names[7], 925, height / 2 + 150);
+	// fill(colours[6]);
+	// ellipse(800, height / 2, sizes[6]);
+	// fill(255);
+	// text(names[6], 800, height / 2 + 150);
+
+	// fill(colours[7]);
+	// ellipse(925, height / 2, sizes[7]);
+	// fill(255);
+	// text(names[7], 925, height / 2 + 150);
 }
