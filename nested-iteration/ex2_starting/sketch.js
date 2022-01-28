d0d9807fc9c2452d336a5df608656bfca21178d5
untari <sastraembun0306@gmail.com>
2022-01-28 13:31:43 +0700

    dynamically adding items to arrays with .push

--- nested-iteration/ex2_starting/sketch.js
+++ nested-iteration/ex2_starting/sketch.js
@@ -1,23 +1,35 @@
 
+var worldCloud;
+var currentWorld;
 
 function setup() 
 {
-    createCanvas(800,800);
+    createCanvas(500,500);
+    worldCloud = [];
+
+    //push method
+    worldCloud.push("first");
+    worldCloud.push("second");
+    worldCloud.push("third");
+    worldCloud.push("fourth");
+
+
+    currentWorld = "";
 }
 
 function draw() 
 {
 
     background(0);
+    fill(255);
 
-
-
-    for(var i = 0; i < 10; i++)
+    for(var i = 0; i < 4; i++)
     {
-        fill(255,0,255);
-        ellipse(50 + i * 50, 50,50,50);
+        text(worldCloud[i], 20, 40 + i * 30);
     }
-    
+}
 
- 
+function keyPressed()
+{
+    console.log(keyCode);
 }
