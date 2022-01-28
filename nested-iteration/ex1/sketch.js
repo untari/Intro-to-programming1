4a130c4ec55352b5bd8701d7be5da24f99b65ac5
untari <sastraembun0306@gmail.com>
2022-01-28 12:58:12 +0700

    start from small grids to bigger grids

--- nested-iteration/ex1/sketch.js
+++ nested-iteration/ex1/sketch.js
@@ -1,5 +1,4 @@
 
-
 function setup() 
 {
     createCanvas(800,800);
@@ -10,15 +9,13 @@ function draw()
 
     background(0);
     fill(255,0,255);
+    noStroke();
 
-    for(var h = 0; h < 3; h++) //rows
+    for(var j = 0; j < 20; j++) //rows
     {
-        for(var i = 0; i < 10; i++) //columns
+        for(var i = 0; i < 20; i++) //columns
         {   
-            fill(255, 0, 255);
-            ellipse(30 + i * 60, 30 + h * 60, 30, 30);
-            fill(225);
-            text("h=" + h + ", i=" + i, 30 + i * 60, 30 + h * 60 );
+            ellipse(50 + i * 30, 50 + j * 30, 5 + i * 2, 5 + i * 2);
         }
     }
  
