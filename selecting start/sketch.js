5d8f4ee72af48f4713679e3ba44e1a9b95e7f761
untari <sastraembun0306@gmail.com>
2022-02-16 22:21:18 +0700

    find the smallest value  index

--- selecting start/sketch.js
+++ selecting start/sketch.js
@@ -8,19 +8,32 @@ function setup()
    }
 
    var highestValue = 0;
-   var nighestIndex = 0;
+   var highestIndex = 0;
+
+   var smallestValue = null;
+   var smallestIndex = 0;
 
    for(var i = 0; i < numArray.length; i++)
    {
       if(numArray[i] > highestValue)
       {
          highestValue = numArray[i];
-         highestValue = i
+         highestValue = i;
+      }
+
+      if(smallestValue = null || numArray[i] < smallestValue)
+      {
+         smallestValue = numArray[i];
+         smallestIndex = i;
       }
    }
 
-   console.log("the highest value in numArray is " + highestValue + "it is index " + highestValue);
+   console.log("the highest value in numArray is " + highestValue + "it is index " + highestIndex);
+
+   console.log("the smallest value in numArray is " + smallestValue + "it is index " + smallestIndex);
+
 }
+
 
 
 
