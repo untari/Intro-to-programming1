f2c1fcbdab412711d9593458bb781a9b65eda261
untari <sastraembun0306@gmail.com>
2022-02-18 15:31:16 +0700

    draw  the point using mult method

--- 9.202/ex1/sketch.js
+++ 9.202/ex1/sketch.js
@@ -10,17 +10,34 @@ function setup()
     tamagotchi = {
 
         points: [],
+        size: 200,
 
         setup: function(){
             // console.log("setup");
             for(var i = 0; i < 36; i++)
             {
-                var v = createVector(0, 200);
+                var incr = PI * 2/36;
+                var v = createVector(0, 1);
+                //rotate the vector
+                var a = incr * i;
+                v.rotate(a);
                 this.points.push(v);
             }
         },
         draw: function(){
             // console.log("draw");
+            fill(255);
+            beginShape();
+            for(var i = 0; i < this.points.length; i++)
+            {
+                var v = p5.Vector.mult(this.points[i], this.size);
+
+                vertex(
+                    v.x,
+                    v.y   
+                )
+            }
+            endShape();
         }
     }
 
